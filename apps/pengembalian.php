ac1dfb6204e27061d41b782166102d4638fb5a4c
mar28 <67575383+marhel28@users.noreply.github.com>
2024-07-14 21:10:16 +0700

    Update pengembalian.php
    edit

--- apps/pengembalian.php
+++ apps/pengembalian.php
@@ -4,7 +4,7 @@ if($_SERVER['REQUEST_METHOD']=='POST'){
     $server = new Server();
     if(isset($_POST['id_mahasiswa'],$_POST['id_buku'])){
         $id_mahasiswa = $_POST['id_mahasiswa'];
-        $id_buku = $_POST['buku_id'];
+        $id_buku = $_POST['id_buku'];
         if(!empty($id_mahasiswa) && !empty($id_buku)){
             $server->pengembalian($id_buku,$id_mahasiswa);
         }
